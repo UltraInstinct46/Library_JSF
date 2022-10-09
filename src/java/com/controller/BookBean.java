40a34eb5c578d6760161415d0bb07583d6b679a0
UltraInstinct46 <restuaerestu291@gmail.com>
2022-10-09 13:33:10 +0700

    Update BookDao

--- src/java/com/controller/BookBean.java
+++ src/java/com/controller/BookBean.java
@@ -1,5 +1,12 @@
 package com.controller;
 
+import com.model.pojo.Book;
+import com.util.HibernateUtil;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +18,37 @@ package com.controller;
  * @author killua
  */
 public class BookBean {
+    private Book book;
+    private Book newbook;
+    private List<Book> DaoAllBooks;
+    private List<Book> DaoSearchBookList;
     
+    public List<Book> AllBooks(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try{
+            session.beginTransaction();
+            DaoAllBooks = session.createCriteria(Book.class).list();
+            int count = DaoAllBooks.size();
+            FacesMessage message1 = new FacesMessage(FacesMessage.SEVERITY_INFO,"List Size", Integer.toString(count));
+            session.getTransaction().commit();
+        }catch(Exception e){
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return DaoAllBooks;
+    }
+    public Integer getId(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String hql = "select max(U.id) from book U";
+        Query query = session.createQuery(hql);
+        List<Integer> results = query.list();
+        Integer userId = 1;
+        if(results.get(0)!= null){
+            userId = results.get(0)+1;
+        }
+        session.flush();
+        session.close();
+        return userId;
+    }
 }
